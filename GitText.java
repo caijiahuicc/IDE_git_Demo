08ac57dad52ad9ba98ac19310c472ad7c76204e5
cc <97387372+caijiahuicc@users.noreply.github.com>
2022-11-07 15:14:19 +0800

    Add files via upload

---
+++ GitText.java
@@ -0,0 +1,8 @@
+package com.git_Demo01;
+
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello hot-fix Git");
+    }
+}
