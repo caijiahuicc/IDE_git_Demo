bbc50e1c729b7dbea7e5adc01dd4f2e12b7dbb7e
cc <97387372+caijiahuicc@users.noreply.github.com>
2022-11-07 15:47:04 +0800

    Update GitText.java

--- GitText.java
+++ GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello hot-fix Git");
+        System.out.println("Change pull！！")
     }
 }
